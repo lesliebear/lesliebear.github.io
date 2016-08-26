b4209b005c3903b79dcae3ceea7c83200d068590
Tian Qi Xiong <tianqi.xiong@gmail.com>
2016-08-26 17:49:21 -0400

    updated for mobile screen/window re-size

--- lxscript.js
+++ lxscript.js
@@ -1,29 +1,46 @@
 $(document).ready(function(){
+	$('#scrolldiv').slimScroll({
+		height: 'auto'
+	});
+
 	$('#About').click(function(e){
 		e.preventDefault();
 		$('#gerbil').hide();
-		$('#gerbildiv').hide();
-		$('#gerbildiv').slimScroll({destroy: true});
+	//	$('#gerbildiv').hide();
+	//	$('#gerbildiv').slimScroll({destroy: true});
 		$('#contact').hide();
 		$('#about').fadeIn('slow','swing');
+
+	/*	if($(".mirror").css("position")=='relative'){
+			$('#aboutdiv').slimScroll({
+				height: 'auto'
+			});
+		}*/
+
+
 	});	
 
 	$('#Gerbil').click(function(e){
 		e.preventDefault();
 		$('#about').hide();
+	//	$('#aboutdiv').hide();
+	//	$('#aboutdiv').slimScroll({destroy:true});
 		$('#contact').hide();
 		$('#gerbildiv').show();
 		$('#gerbil').fadeIn('slow','swing');		
-		$('#gerbildiv').slimScroll({
+	/*	$('#gerbildiv').slimScroll({
 			height: 'auto'
-		});
+		});*/
 	});
 
 	$('#Contact').click(function(e){
 		e.preventDefault();
 		$('#about').hide();
-		$('#gerbildiv').hide();
-		$('#gerbildiv').slimScroll({destroy: true});
+	//	$('#aboutdiv').hide();
+	//	$('#aboutdiv').slimScroll
+	//	$('#aboutdiv').slimScroll{destroy:true});
+	//	$('#gerbildiv').hide();
+	//	$('#gerbildiv').slimScroll({destroy: true});
 		$('#gerbil').hide();
 		$('#contact').fadeIn('slow','swing');
 	});
@@ -31,13 +48,33 @@ $(document).ready(function(){
 	$(".imgSmall").click(function(){
 		var thissrc = $(this).attr('src');
 		$("#imgBig").attr("src", thissrc);
-		$("#overlay").show();
+	//	$("#overlay").show();
 		$("#overlayContent").show();
 	});
 
 	$("#imgBig").click(function(){
 		$("#imgBig").attr("src", "");
-		$("#overlay").hide();
+	//	$("#overlay").hide();
 		$("#overlayContent").hide();
 	});
+
+	$(window).resize(function(){
+		var h=$('.mirror').height();
+		$('#scrolldiv').height(h);
+		$('.slimScrollDiv').height(h);
+	});	
+
+	$('.destroy').click(function(){
+		$('.scrollable').slimScroll({
+			destroy:true
+		});
+
+		var $elem = $('.scrollable'),
+		events = jQuery._data( $elem[0], "events" );
+
+		if (events) {
+			jQuery._removeData( $elem[0], "events" );
+		}
+	});
+	
 });
